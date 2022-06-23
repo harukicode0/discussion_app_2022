afbc76f704d9a70da774b80eb80a36f27d7ed016
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-23 18:55:15 +0900

    roomのshow作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ApplicationRecord
   has_many :users, through: :user_room
   has_many :comments
+
+  with_options presence: true do
+    validates :name
+    validates :maker_user
+  end
 end
