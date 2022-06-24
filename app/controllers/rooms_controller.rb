f852fe3ee0c5faeb7f4326295cbab1fab10e58d9
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-24 12:52:57 +0900

    ポジション表明のデータハンドリング

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -26,7 +26,25 @@ class RoomsController < ApplicationController
   end
 
   def standing_position
-    redirect_to action: :show
+    #ルームを探す
+    @room = Room.find(params[:room_id])
+     #中間テーブルが存在するか否かの条件式
+    if @user_room = UserRoom.find_by(room_id: @room.id, user_id: current_user.id)
+      #既にポジションがある場合はアップデート、ない場合は新規作成
+      if @position = Position.find_by(user_room_id:@user_room.id)
+        #ポジションがある場合
+        @position.update(user_room_id: @user_room.id, standing_position_id:params[:standing_position]) 
+        redirect_to action: :show
+      else
+        #ポジションがない場合
+        @position = Position.create(user_room_id: @user_room.id, standing_position_id:params[:standing_position])
+        redirect_to action: :show
+      end
+    else
+      @user_room = UserRoom.create(room_id: @room.id, user_id: current_user.id) #中間テーブルを作成
+      @position = Position.create(user_room_id: @user_room.id, standing_position_id:params[:standing_position])  #ポジションを作成
+      redirect_to action: :show
+    end
   end
 
   private
