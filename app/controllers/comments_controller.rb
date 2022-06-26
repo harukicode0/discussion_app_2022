a561e4bdc107db7c5bca8f6dfe29eb8c0111a4dd
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-26 18:21:58 +0900

    ポジションによるコメントの表示位置変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,19 @@
 class CommentsController < ApplicationController
   def create
-    find_user_positioin
-    binding.pry
-    
-    comment = Comment.new(comment_params)
     @room = Room.find(params[:room_id])
-    if comment.save
-      redirect_to room_path(@room)
+    if find_user_positioin
+      comment = Comment.new(comment_params)
+      
+      if comment.save
+        redirect_to room_path(@room)
+      else
+        @comment = Comment.new
+        @comments = @room.comments.includes(:user)
+        render 'rooms/show'
+      end
     else
       @comment = Comment.new
+      @comments = @room.comments.includes(:user)
       render 'rooms/show'
     end
   end
@@ -16,6 +21,12 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id:current_user.id, room_id:params[:room_id], position_id:)
+    params.require(:comment).permit(:text).merge(user_id:current_user.id, room_id:params[:room_id],standing_position_id:@position.standing_position_id)
+  end
+
+  def find_user_positioin
+    if @user_room = UserRoom.find_by(user_id:current_user.id,room_id:@room.id)
+      @position = @user_room.position
+    end
   end
 end
