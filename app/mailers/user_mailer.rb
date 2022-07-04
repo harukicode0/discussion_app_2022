e6464a82032ab4e83e5ba6058d9830e20d769af0
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-04 14:53:43 +0900

    mailerアクションの作成

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
-  layout 'mailer'
+  default from: 'notifications@example.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: '私の素敵なサイトへようこそ')
+  end
 end
