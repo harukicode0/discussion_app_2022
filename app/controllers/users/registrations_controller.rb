923672b940acd3ee0b55ff3146d326daa7e6e390
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-04 15:31:39 +0900

    devise:userコントローラーの作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,15 +3,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  before_action :authenticate_user!
-
-  private
-
-  def after_update_path_for(resource)
-    if user_signed_in? && current_user == resource
-      user_path(current_user)
-    end
-  end
 
   # GET /resource/sign_up
   # def new
