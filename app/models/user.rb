6c5d2c5ccfc74725d0026fa02805f8cfc5640e51
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-05 02:33:06 +0900

    ストロングパラメーターの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   has_many :owner_rooms, class_name: 'Room'
   has_many :comments,dependent: :destroy
   has_many :likes,dependent: :destroy
+  has_one_attached :image
 
   #フォロ機能のアソシエーション
   has_many :relationships, foreign_key: :following_id
