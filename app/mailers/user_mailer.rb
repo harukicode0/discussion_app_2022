97e595bba36dc7ea7f3fdc91f7d0efe8a15e29d4
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-04 15:59:18 +0900

    メアド送信仮作成

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def welcome_email(user)
-    @user = user
+  def welcome_email
+    @user = params[:user]
     mail(to: @user.email, subject: '登録ありがとうございます')
   end
 end
