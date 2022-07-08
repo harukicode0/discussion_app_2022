19cbe10e950be622e1c317021849daa37508b2ca
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-08 16:59:45 +0900

    issueコントローラー作成

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,3 +1,21 @@
 class IssuesController < ApplicationController
+  def new
+    @issue = Issue.new
+    @room = Room.find(params[:room_id])
+  end
+  
+  def create
+    @issue = Issue.new(issue_params)
+    if @issue.save
+      redirect_to room_path(params[:room_id])
+    else
+      @room = Room.find(params[:room_id])
+      render 'new'
+    end
+  end
 
+  private
+  def issue_params
+    params.require(:issue).permit(:issue_title).merge(room_id: params[:room_id])
+  end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,6 @@
 class Issue < ApplicationRecord
   belongs_to :room
   has_many :issue_comments, dependent: :destroy
+
+  validates :issue_title, presence: true
 end
