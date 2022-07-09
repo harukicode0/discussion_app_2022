34b4a183ebedc96714bab1dd269100ca161e6316
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-09 13:46:27 +0900

    微修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -93,7 +93,7 @@ class RoomsController < ApplicationController
   private
 
   def room_other_params
-    params.require(:room_tag_user_form).permit(:title,:tag_name).merge(deadline: Time.now + 3.days, user_id: current_user.id)
+    params.require(:room_tag_user_form).permit(:title,:tag_name).merge(deadline: Time.now + 5.days, user_id: current_user.id)
   end
 
   def create_new_position
