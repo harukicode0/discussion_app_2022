8ed2fcc466ccd0161d62691c2827bbeb268fcb9d
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-11 03:49:11 +0900

    検索jsの修正・完成

--- app/javascript/search-box.js
+++ app/javascript/search-box.js
@@ -1,10 +1,19 @@
 function search_box() {
   const search_box = document.querySelector("#search-box");
   if (!search_box) return null;
-  const target = document.querySelector(".search-form");
-  search_box.addEventListener("click", function () {
-    target.classList.remove("search-form");
-    target.classList.add("is-fixed");
+  const line = document.querySelector(".search-form");
+  // ページをクリックした時の処理
+  document.addEventListener("click", (e) => {
+    // クリックした要素（"#search-box"）の親要素を判定している
+    if (e.target.closest("#search-box")) {
+      // "#search-box";がある場合の処理
+      line.classList.remove("search-form");
+      line.classList.add("is-fixed");
+    } else {
+      // "#search-box";がない場合の処理
+      line.classList.add("search-form");
+      line.classList.remove("is-fixed");
+    }
   });
 }
 
