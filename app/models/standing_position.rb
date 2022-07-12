c25ecb55229b5da73802d259da27712051ec91c2
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-13 08:20:06 +0900

    room下部完成

--- app/models/standing_position.rb
+++ app/models/standing_position.rb
@@ -1,8 +1,8 @@
 class StandingPosition < ActiveHash::Base
   self.data = [
     { id: 0, name: '中立' },
-    { id: 1, name: '賛成' },
-    { id: 2, name: '反対' }
+    { id: 1, name: 'Yes' },
+    { id: 2, name: 'No' }
   ]
 
   include ActiveHash::Associations
