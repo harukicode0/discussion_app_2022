46e6f2dc59c25fd53381492ff8ee46df2f140e0d
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-20 20:53:10 +0900

    issue矢印部分にjs対応

--- app/javascript/issue-appear.js
+++ app/javascript/issue-appear.js
@@ -2,8 +2,10 @@ function issue_appear() {
   const ronten = document.getElementById("room-ronten");
   if (!ronten) return null;
   const room_issue = document.querySelector(".room-issues-right");
+  const yazirusi = document.querySelector(".comment-yazirusi");
   ronten.addEventListener("click", function () {
     room_issue.classList.toggle("room-issues-right-set");
+    yazirusi.classList.toggle("comment-yazirusi-set");
   });
 }
 
