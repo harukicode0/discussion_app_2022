7781db15ba932466f2f201b7833f72e994f80ba4
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-21 04:32:01 +0900

    tag編集機能実装

--- app/models/room_tag_user_form.rb
+++ app/models/room_tag_user_form.rb
@@ -16,8 +16,22 @@ class RoomTagUserForm
   end
 
   def update(params,room)
+    #一度タグの紐付けを消す
+
+    
+    room.room_tags.destroy_all
+
+    #paramsの中のタグの情報を削除。同時に、返り値としてタグの情報を変数に代入
+    tag_name = params.delete(:tag_name)
+
+     #もしタグの情報がすでに保存されていればインスタンスを取得、無ければインスタンスを新規作成
+    tag = Tag.where(tag_name: tag_name).first_or_initialize if tag_name.present?
+
+    #タグを保存
+    tag.save if tag_name.present?
+    RoomTag.create(room_id: room.id, tag_id: tag.id) if tag_name.present?
+
     room.update(title: title)
-    Tag.update(tag_name: tag_name) if tag_name != ""
     @room_id = room.id
   end
 end
