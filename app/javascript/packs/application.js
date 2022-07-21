53365b85c8c2d3d9450db03af519e2e8362c1eb8
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-22 04:48:11 +0900

    ルーム情報js実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@ require("../sort-location");
 require("../position-color");
 require("../comment-big");
 require("../issue-appear");
+require("../room-dropdown");
 // require("../ajax");
 
 Rails.start();
