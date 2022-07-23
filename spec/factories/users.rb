591e3dfe9704cd678b4360917d6c6d492e414f0f
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-24 08:12:45 +0900

    userのfactory完成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    after(:build) do |message|
+      user.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
