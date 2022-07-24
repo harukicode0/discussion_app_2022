a05a2f30a9177c5e3aa57cbafb863cf50fc7d3f2
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-24 15:52:31 +0900

    user-factoryを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
-    after(:build) do |message|
+    after(:build) do |user|
       user.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
