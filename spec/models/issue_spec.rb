c571ac95f9759226137c5549532b7a7fab73d61c
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-25 01:47:50 +0900

    issue-test-code maked

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
+# bundle exec rspec spec/models/issue_spec.rb
 
 RSpec.describe Issue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @issue = FactoryBot.build(:issue)
+  end
+
+  describe 'チャットルーム作成' do
+    context '新規作成できる場合' do
+      it "titleがあれば作成できる" do
+        expect(@issue).to be_valid
+      end
+    end
+    context '新規作成できない場合' do
+      it "titleが空では作成できない" do
+        @issue.issue_title = ''
+        @issue.valid?
+        expect(@issue.errors.full_messages).to include("Issue titleを入力してください")
+      end
+      it "roomが紐づいていないと作成できない" do
+        @issue.room = nil
+        @issue.valid?
+        expect(@issue.errors.full_messages).to include("Roomを入力してください")
+      end
+      it "owner_idが空だと作成できない" do
+        @issue.owner_id = ''
+        @issue.valid?
+        expect(@issue.errors.full_messages).to include("Ownerを入力してください")
+      end
+    end
+  end
 end
