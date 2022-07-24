33860ac0865ea26d1ad46c8dee80a1b3e34c7adc
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-24 18:47:59 +0900

    deadlineデータの受け渡し方法変更

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -119,7 +119,7 @@ class RoomsController < ApplicationController
   private
 
   def room_other_params
-    params.require(:room_tag_user_form).permit(:title,:tag_name,:days).merge(user_id: current_user.id)
+    params.require(:room_tag_user_form).permit(:title,:tag_name,:days).merge(deadline: Time.now + params[:room_tag_user_form][:days].to_i.days, user_id: current_user.id)
   end
 
   def create_new_position
