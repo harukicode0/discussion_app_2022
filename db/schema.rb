6ad0ec6dfa346b50417bd91517ac0e76860e901f
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-30 01:07:50 +0900

    replyテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_175848) do
+ActiveRecord::Schema.define(version: 2022_07_29_160441) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2022_07_08_175848) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comment_replies", charset: "utf8", force: :cascade do |t|
+    t.string "text"
+    t.bigint "comment_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["comment_id"], name: "index_comment_replies_on_comment_id"
+    t.index ["user_id"], name: "index_comment_replies_on_user_id"
   end
 
   create_table "comments", charset: "utf8", force: :cascade do |t|
@@ -158,6 +168,8 @@ ActiveRecord::Schema.define(version: 2022_07_08_175848) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comment_replies", "comments"
+  add_foreign_key "comment_replies", "users"
   add_foreign_key "comments", "rooms"
   add_foreign_key "comments", "users"
   add_foreign_key "issue_comments", "issues"
