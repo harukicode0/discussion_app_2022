1b2d3c535896b857c9cf87ec56990e7f97faaebf
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-07-30 01:11:36 +0900

    controller作成

---
+++ spec/helpers/comment_reply_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the CommentReplyHelper. For example:
+#
+# describe CommentReplyHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe CommentReplyHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
